97aec28017e4b77f0ded5b01afb7c19bb9c18929
Michael Aerni <hello@michaelaerni.ch>
2026-09-17 09:40:36 +0200

    Align CP assets with Statamic 6 Vite and fieldtype patterns.
    Use the official Tailwind entrypoint and Fieldtype composable, and harden Places with cleanup and read-only support.

--- resources/js/components/Places.vue
+++ resources/js/components/Places.vue
@@ -1,8 +1,192 @@
+<script setup>
+import { Fieldtype } from '@statamic/cms';
+import { onBeforeUnmount, onMounted, useTemplateRef, watch } from 'vue';
+
+const emit = defineEmits(Fieldtype.emits);
+const props = defineProps(Fieldtype.props);
+const { expose, update, isReadOnly } = Fieldtype.use(emit, props);
+defineExpose(expose);
+
+const mapEl = useTemplateRef('map');
+const autocompleteEl = useTemplateRef('autocomplete');
+
+let map = null;
+let marker = null;
+let infoWindow = null;
+let placeAutocomplete = null;
+let closeClickListener = null;
+
+onMounted(() => {
+    init();
+});
+
+onBeforeUnmount(() => {
+    destroy();
+});
+
+watch(isReadOnly, (readOnly) => {
+    setInteractive(!readOnly);
+});
+
+async function init() {
+    await google.maps.importLibrary('marker');
+    await google.maps.importLibrary('places');
+
+    initMap();
+    initPlaceAutocomplete();
+    setInteractive(!isReadOnly.value);
+
+    if (props.value) {
+        const place = new google.maps.places.Place({ id: props.value });
+        await place.fetchFields({ fields: ['displayName', 'formattedAddress', 'location', 'viewport'] });
+
+        updateMapInfo(place);
+    }
+}
+
+function initMap() {
+    map = new google.maps.Map(mapEl.value, {
+        mapId: props.meta.map_id,
+        center: props.meta.center,
+        zoom: props.meta.zoom,
+        mapTypeControl: false,
+        fullscreenControl: false,
+        streetViewControl: false,
+        cameraControl: false,
+        zoomControl: true,
+    });
+}
+
+function initPlaceAutocomplete() {
+    placeAutocomplete = new google.maps.places.PlaceAutocompleteElement({
+        includedRegionCodes: props.meta.countries,
+        locationBias: props.meta.center,
+    });
+
+    autocompleteEl.value.appendChild(placeAutocomplete);
+
+    placeAutocomplete.addEventListener('gmp-select', onPlaceSelect);
+}
+
+async function onPlaceSelect({ placePrediction }) {
+    if (isReadOnly.value) {
+        return;
+    }
+
+    const place = placePrediction.toPlace();
+    await place.fetchFields({ fields: ['displayName', 'formattedAddress', 'location', 'viewport'] });
+
+    updateMapInfo(place);
+
+    update(place.id);
+}
+
+function updateMapInfo(place) {
+    createMarker(place.location);
+    createInfoWindow(place.displayName, place.formattedAddress, place.location);
+
+    placeAutocomplete.locationBias = place.location;
+
+    if (place.viewport) {
+        map.fitBounds(place.viewport);
+    } else {
+        map.setCenter(place.location);
+        map.setZoom(17);
+    }
+}
+
+function createMarker(location) {
+    if (marker) {
+        marker.map = null;
+    }
+
+    marker = new google.maps.marker.AdvancedMarkerElement({
+        map,
+    });
+
+    marker.position = location;
+}
+
+function createInfoWindow(displayName, formattedAddress, location) {
+    clearInfoWindow();
+
+    infoWindow = new google.maps.InfoWindow();
+
+    infoWindow.setHeaderContent(displayName);
+    infoWindow.setContent(formattedAddress);
+    infoWindow.setPosition(location);
+
+    infoWindow.open({
+        map,
+        anchor: marker,
+        shouldFocus: false,
+    });
+
+    closeClickListener = infoWindow.addListener('closeclick', () => destroyInfoWindow());
+}
+
+function destroyInfoWindow() {
+    if (isReadOnly.value) {
+        return;
+    }
+
+    if (marker) {
+        marker.map = null;
+        marker = null;
+    }
+
+    clearInfoWindow();
+    update(null);
+}
+
+function clearInfoWindow() {
+    if (closeClickListener) {
+        google.maps.event.removeListener(closeClickListener);
+        closeClickListener = null;
+    }
+
+    if (infoWindow) {
+        infoWindow.close();
+        infoWindow = null;
+    }
+}
+
+function setInteractive(interactive) {
+    if (placeAutocomplete) {
+        placeAutocomplete.disabled = !interactive;
+    }
+
+    if (map) {
+        map.setOptions({
+            gestureHandling: interactive ? 'auto' : 'none',
+            zoomControl: interactive,
+            keyboardShortcuts: interactive,
+        });
+    }
+}
+
+function destroy() {
+    clearInfoWindow();
+
+    if (placeAutocomplete) {
+        placeAutocomplete.removeEventListener('gmp-select', onPlaceSelect);
+        placeAutocomplete.remove();
+        placeAutocomplete = null;
+    }
+
+    if (marker) {
+        marker.map = null;
+        marker = null;
+    }
+
+    map = null;
+}
+</script>
+
 <template>
-    <div class="gm:relative gm:overflow-hidden gm:rounded-lg gm:h-[600px] gm:border gm:border-gray-300">
-        <div ref="map" class="gm:h-full"></div>
-        <div ref="autocomplete" class="autocomplete"></div>
-        <div ref="autocomplete" class="gm:rounded-[3px] shadow-ui-sm gm:border gm:border-gray-300 gm:absolute gm:right-4 gm:left-4 gm:top-4"></div>
+    <div class="relative overflow-hidden rounded-lg h-[600px] border border-gray-300" :class="{ 'pointer-events-none opacity-75': isReadOnly }">
+        <div ref="map" class="h-full"></div>
+        <div ref="autocomplete" class="rounded-[3px] shadow-ui-sm border border-gray-300 absolute right-4 left-4 top-4"></div>
     </div>
 </template>
 
@@ -31,121 +215,3 @@
         font-weight: 400;
     }
 </style>
-
-<script>
-import { FieldtypeMixin as Fieldtype } from '@statamic/cms';
-
-export default {
-    mixins: [Fieldtype],
-
-    data() {
-        return {
-            map: null,
-            marker: null,
-            infoWindow: null,
-            placeAutocomplete: null,
-        };
-    },
-
-    mounted() {
-        this.init();
-    },
-
-    methods: {
-        async init() {
-            await google.maps.importLibrary("marker")
-            await google.maps.importLibrary("places")
-
-            this.initMap();
-            this.initPlaceAutocomplete();
-
-            if (this.value) {
-                const place = new google.maps.places.Place({ id: this.value });
-                await place.fetchFields({ fields: ['displayName', 'formattedAddress', 'location', 'viewport'] });
-
-                this.updateMapInfo(place);
-            }
-        },
-
-        initMap() {
-            this.map = new google.maps.Map(this.$refs.map, {
-                mapId: this.meta.map_id,
-                center: this.meta.center,
-                zoom: this.meta.zoom,
-                mapTypeControl: false,
-                fullscreenControl: false,
-                streetViewControl: false,
-                cameraControl: false,
-                zoomControl: true,
-            });
-        },
-
-        initPlaceAutocomplete() {
-            this.placeAutocomplete = new google.maps.places.PlaceAutocompleteElement({
-                includedRegionCodes: this.meta.countries,
-                locationBias: this.meta.center,
-            });
-
-            this.$refs.autocomplete.appendChild(this.placeAutocomplete);
-
-            this.placeAutocomplete.addEventListener('gmp-select', async ({ placePrediction }) => {
-                const place = placePrediction.toPlace();
-                await place.fetchFields({ fields: ['displayName', 'formattedAddress', 'location', 'viewport'] });
-
-                this.updateMapInfo(place);
-
-                this.update(place.id);
-            });
-        },
-
-        updateMapInfo(place) {
-            this.createMarker(place.location);
-            this.createInfoWindow(place.displayName, place.formattedAddress, place.location);
-
-            this.placeAutocomplete.locationBias = place.location;
-
-            if (place.viewport) {
-                this.map.fitBounds(place.viewport);
-            } else {
-                this.map.setCenter(place.location);
-                this.map.setZoom(17);
-            }
-        },
-
-        createMarker(location) {
-            if (this.marker) {
-                this.marker.map = null;
-            }
-
-            this.marker = new google.maps.marker.AdvancedMarkerElement({
-                map: this.map,
-            });
-
-            this.marker.position = location;
-        },
-
-        createInfoWindow(displayName, formattedAddress, location) {
-            this.infoWindow = new google.maps.InfoWindow();
-
-            this.infoWindow.setHeaderContent(displayName);
-            this.infoWindow.setContent(formattedAddress);
-            this.infoWindow.setPosition(location);
-
-            this.infoWindow.open({
-                map: this.map,
-                anchor: this.marker,
-                shouldFocus: false,
-            });
-
-            this.infoWindow.addListener("closeclick", () => this.destroyInfoWindow());
-        },
-
-        destroyInfoWindow() {
-            if (this.marker) {
-                this.marker.map = null;
-                this.update(null);
-            }
-        }
-    },
-};
-</script>
